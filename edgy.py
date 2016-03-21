d603232045160b0ff3eaa3d21fefdd8e9a1d212e
Diptodip Deb <d2d.tar.gz@gmail.com>
2016-03-20 23:58:51 -0400

    Update .gitignore.

--- edgy.py
+++ edgy.py
@@ -5,15 +5,28 @@ import choose
 import count
 
 def edgy(image):
+    # show original image
     cv2.imshow("Image", image)
+
+    # preprocess image
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (1, 1), 0)
+
+    # determine background color and maximum brightness
     minimum = choose.adjusted_average(image)
     maximum = 120
+
+    # perform Canny edge detection
     edges = cv2.Canny(gray, minimum, maximum)
+
+    # perform morphological closing to solidify spots
     kernel = np.ones((3, 3), np.uint8)
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+    # show image after closing
     cv2.imshow("Edge", closed)
+
+    # count spots and show output
     objects = count.segment(image, closed)
     print("[out] Found {} objects.".format(objects))
     cv2.waitKey(0)
