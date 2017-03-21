6c6e700363c395f9269207367ed049f7725d6cb1
Diptodip Deb <d2d.tar.gz@gmail.com>
2017-03-21 02:06:03 -0400

    Decent preliminary generation without deep learning

--- generator/generate_image.py
+++ generator/generate_image.py
@@ -6,29 +6,36 @@ import numpy as np
 from tifffile import imsave
 
 GRID_SIZE = 32
-AVERAGE_BRIGHTNESS = 2000
-STDDEV = 50
+AVERAGE_BRIGHTNESS = 2500
+STDDEV = 250
 
-def generate_image():
-    NUM_SPOTS = randint(5, 90)
-    NUM_FRAMES = randint(40, 90)
+def generate_image(filename = 'test.tiff'):
+    NUM_SPOTS = randint(25, 40)
+    NUM_FRAMES = randint(18, 32)
     I = np.random.normal(AVERAGE_BRIGHTNESS, STDDEV, (NUM_FRAMES, GRID_SIZE, GRID_SIZE)).astype(np.uint16)
     for spot in range(NUM_SPOTS):
         generate_spot(I, NUM_FRAMES)
     I = np.reshape(I, (NUM_FRAMES, GRID_SIZE, GRID_SIZE), order='A')
     print(I.shape)
-    imsave('test.tiff', I)
+    print(NUM_SPOTS)
+    imsave(filename, I)
 
 def generate_spot(I, NUM_FRAMES):
-    max_brightness = randint(2500, 4000)
-    max_radius_x = randint(1, 3)
-    max_radius_y = randint(1, 3)
-    x = randint(0, GRID_SIZE - 1)
-    y = randint(0, GRID_SIZE - 1)
+    max_brightness = 0
+    while not (max_brightness > 3000):
+        max_brightness = int(round(np.random.normal(2.5 * AVERAGE_BRIGHTNESS, STDDEV)))
+    max_radius_x = int(round(np.random.normal(2, 1.0)))
+    max_radius_y = int(round(np.random.normal(2, 1.0)))
+    while not (max_radius_x > 1 and max_radius_y > 1):
+        max_radius_x = int(round(np.random.normal(2, 1.5)))
+        max_radius_y = int(round(np.random.normal(2, 1.5)))
+    x = abs(min(int(round(np.random.normal(GRID_SIZE / 2.0, 0.3 * GRID_SIZE))), GRID_SIZE - 1))
+    y = abs(min(int(round(np.random.normal(GRID_SIZE / 2.0, 0.3 * GRID_SIZE))), GRID_SIZE - 1))
     z = randint(0, NUM_FRAMES - 1)
-    frame_range = randint(5, 12)
-    median_frame = int(round(median(range(frame_range))))
-    for i in range(frame_range):
+    print((x, y, z))
+    frame_radius = randint(9, 16)
+    max_distance = sqrt(max_radius_x**2 + max_radius_y**2)
+    for i in range(-frame_radius, frame_radius):
         for j in range(-max_radius_x, max_radius_x):
             for k in range(-max_radius_y, max_radius_y):
                 a = x + j
@@ -37,11 +44,12 @@ def generate_spot(I, NUM_FRAMES):
                 b = y + k
                 if b < 0: b = 0
                 if b >= GRID_SIZE: b = GRID_SIZE - 1
-                if z + i >= NUM_FRAMES - 1:
+                if z + i > NUM_FRAMES - 1:
                     break
                 distance = sqrt(j**2 + k**2)
-                brightness = randint(0, max_brightness - AVERAGE_BRIGHTNESS)
-                brightness = int(round(brightness * (1 - float(distance) / max_radius_x) * (1 - abs(median_frame - i) / float(median_frame)))) + AVERAGE_BRIGHTNESS
-                I[z + i, a, b] = brightness
+                brightness = randint(int((max_brightness - AVERAGE_BRIGHTNESS) / 2), max_brightness - AVERAGE_BRIGHTNESS)
+                brightness *= (1 - distance/max_distance)
+                brightness *= (1 - abs(i) / frame_radius)
+                I[z + i, a, b] += brightness
 
 if __name__=='__main__': generate_image()
