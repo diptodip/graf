5b61e29caedd8c24b8ce89316345b2021a3f6f4f
Diptodip Deb <d2d.tar.gz@gmail.com>
2017-04-11 03:58:47 -0400

    Generate solution stack concurrently to image stack

--- generator/generate_image.py
+++ generator/generate_image.py
@@ -9,18 +9,20 @@ GRID_SIZE = 32
 AVERAGE_BRIGHTNESS = 2500
 STDDEV = 250
 
-def generate_image(filename = 'test.tiff'):
+def generate_image(file_prefix = 'test'):
     NUM_SPOTS = randint(25, 40)
     NUM_FRAMES = randint(18, 32)
     I = np.random.normal(AVERAGE_BRIGHTNESS, STDDEV, (NUM_FRAMES, GRID_SIZE, GRID_SIZE)).astype(np.uint16)
+    I_ = np.zeros((NUM_FRAMES, GRID_SIZE, GRID_SIZE)).astype(np.uint16)
     for spot in range(NUM_SPOTS):
-        generate_spot(I, NUM_FRAMES)
+        generate_spot(I, I_, NUM_FRAMES)
     I = np.reshape(I, (NUM_FRAMES, GRID_SIZE, GRID_SIZE), order='A')
     print(I.shape)
     print(NUM_SPOTS)
-    imsave(filename, I)
+    imsave(file_prefix + '.tiff', I)
+    imsave(file_prefix + '_label.tiff', I_)
 
-def generate_spot(I, NUM_FRAMES):
+def generate_spot(I, I_, NUM_FRAMES):
     max_brightness = 0
     while not (max_brightness > 3000):
         max_brightness = int(round(np.random.normal(2.5 * AVERAGE_BRIGHTNESS, STDDEV)))
@@ -35,6 +37,7 @@ def generate_spot(I, NUM_FRAMES):
     print((x, y, z))
     frame_radius = randint(9, 16)
     max_distance = sqrt(max_radius_x**2 + max_radius_y**2)
+    I_[z, x, y]  = 6000
     for i in range(-frame_radius, frame_radius):
         for j in range(-max_radius_x, max_radius_x):
             for k in range(-max_radius_y, max_radius_y):
