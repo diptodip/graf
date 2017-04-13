f30ca17a0c792b392c684b546644266449c483c8
Diptodip Deb <d2d.tar.gz@gmail.com>
2017-04-13 09:17:29 -0400

    Generate density map instead of label

--- convnet/convnet.py
+++ convnet/convnet.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-from generator.normal_approximation import *
+from normal_approximation import *
+
+sess = tf.InteractiveSession()
 
 def standardize(frame):
     return tf.image.per_image_standardization(frame)
@@ -22,7 +24,7 @@ def weight_variable(shape):
     and initialized to values drawn from a Gaussian distribution
     with standard deviation of 0.1.
     """
-    initial = tf.truncated_normal(shape, stdev=0.1)
+    initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 def bias_variable(shape):
@@ -52,25 +54,26 @@ def max_pool(x):
     """
     Specifies a 2x2 max pooling operation on the input x.
     """
-    return tf.nn.max_pool(x, k_size = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 
 """
 Construction of the computation graph.
 """
 
-x = tf.placeholder(tf.float32)
-y_ = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32, shape = [None, 1024])
+y_ = tf.placeholder(tf.float32, shape = [None, 1024])
 
-W_conv1 = weight_variable([3 3 1 32])
+W_conv1 = weight_variable([3, 3, 1, 32])
 b_conv1 = bias_variable([32])
 
 # Format the input array as a TensorFlow image array.
-x_image = tf.reshape(x, [-1, 32, 32, 1])
+x_image = tf.image.per_image_standardization(tf.reshape(x, [32, 32, 1])) * 100
+x_stdimg = tf.reshape(x_image, [-1, 32, 32, 1])
 
-h_conv1 = tf.nn.relu(convolve(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(convolve(x_stdimg, W_conv1) + b_conv1)
 h_pool1 = max_pool(h_conv1)
 
-W_conv2 = weight_variable([3 3 32 64])
+W_conv2 = weight_variable([3, 3, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(convolve(h_pool1, W_conv2) + b_conv2)
@@ -78,40 +81,51 @@ h_pool2 = max_pool(h_conv2)
 
 # Note that ''middle'' refers to the connecting fully connected layer,
 # though this is actually implemented as another convolution.
-W_middle = weight_variable([3 3 64 256])
+W_middle = weight_variable([3, 3, 64, 256])
 b_middle = bias_variable([256])
 
 h_middle = tf.nn.relu(convolve(h_pool2, W_middle) + b_middle)
 
-W_unconv1 = weight_variable([3 3 256 64])
+W_unconv1 = weight_variable([3, 3, 256, 64])
 b_unconv1 = bias_variable([64])
 
 h_unconv1 = convolve(tf.nn.relu(upsample(h_middle, [16, 16])), W_unconv1) + b_unconv1
 
-W_unconv2 = weight_variable([3 3 64 32])
+W_unconv2 = weight_variable([3, 3, 64, 32])
 b_unconv2 = bias_variable([32])
 
-h_unconv2 = convolve(tf.nn.relu(upsample(h_middle, [32, 32])), W_unconv2) + b_unconv2
+h_unconv2 = convolve(tf.nn.relu(upsample(h_unconv1, [32, 32])), W_unconv2) + b_unconv2
 
 # This is the final output convolution, hence no pooling is used.
-W_outconv = weight_variable([3 3 32 1])
+W_outconv = weight_variable([3, 3, 32, 1])
 b_outconv = bias_variable([1])
 
 y_image = tf.nn.relu(convolve(h_unconv2, W_outconv) + b_outconv)
-y_flat = tf.reshape(y_image, [1024])
+y_png = tf.image.encode_png(tf.cast(tf.reshape(y_image, [32, 32, 1]) * 100, tf.uint8))
+y_flat = tf.reshape(y_image, [-1, 1024])
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits = y_image))
+# cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits = y_flat))
+cross_entropy = tf.reduce_mean(tf.squared_difference(y_flat, tf.reshape(tf.image.per_image_standardization(tf.reshape(y_, [32, 32, 1])), [-1, 1024])))
 
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_flat, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+correct_prediction = tf.equal(tf.argmax(y_flat, axis=1), tf.argmax(y_, axis=1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
 sess.run(tf.global_variables_initializer())
+tf.set_random_seed(413)
 
 for i in range(100000):
     I, I_, num_frames = generate_image()
+    if i % 100 == 0:
+        print('step {}'.format(i))
+    accuracies = []
     for j in range(num_frames):
         if i%100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x: I[j,:,:], y_: tf.reshape(I_[j,:,:], [1024])})
-            print(y_image)
-            print("step %d, training accuracy %g"%(i, train_accuracy))
-        train_step.run(feed_dict={x: I[j,:,:], y_: tf.reshape(I_[j,:,:], [1024])})
+            train_accuracy, loss, png = sess.run([accuracy, cross_entropy, y_png], feed_dict = {x: np.reshape(I[j,:,:], (1, 1024)), y_: np.reshape(I_[j,:,:], (1, 1024))})
+            # train_accuracy = accuracy.eval(feed_dict={x: np.reshape(I[j,:,:], (1, 1024)), y_: np.reshape(I_[j,:,:], (1, 1024))})
+            print("frame %d: loss %f"%(j, loss))
+            accuracies.append(train_accuracy)
+            with open('test{}.png'.format(j), 'wb+') as f:
+                f.write(png)
+        train_step.run(feed_dict={x: np.reshape(I[j,:,:], (1, 1024)), y_: np.reshape(I_[j,:,:], (1, 1024))})
+    if i%100 == 0:
+        print('=====================================')
---
+++ convnet/normal_approximation.py
@@ -0,0 +1,79 @@
+from random import randint
+from statistics import median
+from math import sqrt
+
+import numpy as np
+from PIL import Image
+import cv2
+
+GRID_SIZE = 32
+AVERAGE_BRIGHTNESS = 2500
+STDDEV = 250
+
+def generate_image(file_prefix = 'test', save_images = True):
+    """
+    Generate an image stack resembling a patch of a 3D image as found in
+    smFISH microscopy and save it as a NumPy nd-array with a file name specified
+    by file_prefix. Alternatively, save the frames of the stack individually as
+    image files.
+    """
+    NUM_SPOTS = randint(25, 40)
+    NUM_FRAMES = randint(18, 32)
+    I = np.random.normal(AVERAGE_BRIGHTNESS, STDDEV, (NUM_FRAMES, GRID_SIZE, GRID_SIZE)).astype(np.uint16)
+    I_ = np.zeros((NUM_FRAMES, GRID_SIZE, GRID_SIZE)).astype(np.uint16)
+    for spot in range(NUM_SPOTS):
+        generate_spot(I, I_, NUM_FRAMES)
+    I = np.reshape(I, (NUM_FRAMES, GRID_SIZE, GRID_SIZE), order='A')
+    I_ = np.reshape(I_, (NUM_FRAMES, GRID_SIZE, GRID_SIZE), order='A')
+    if not save_images:
+        np.save(file_prefix, I)
+        np.save(file_prefix + '_label', I_)
+    else:
+        for z in range(NUM_FRAMES):
+            # frame = Image.fromarray(np.reshape(I[z,:,:], (32, 32)))
+            # frame.save(file_prefix + '_{}.tiff'.format(z))
+            # label = Image.fromarray(I_[z,:,:])
+            # label.save(file_prefix + '_{}_label.tiff'.format(z))
+            cv2.imwrite(file_prefix + '_{}.tiff'.format(z), I[z,:,:])
+            cv2.imwrite(file_prefix + '_{}_label.tiff'.format(z), I_[z,:,: ])
+    return (I, I_, NUM_FRAMES)
+
+def generate_spot(I, I_, NUM_FRAMES):
+    """
+    Generate a spot on a specific frame along with its label.
+    The spot is placed in the appropriate frames of I and the
+    label for the spot is placed in the corresponding center frame
+    for the spot.
+    """
+    max_brightness = 0
+    while not (max_brightness > 3000):
+        max_brightness = int(round(np.random.normal(2.5 * AVERAGE_BRIGHTNESS, STDDEV)))
+    max_radius_x = int(round(np.random.normal(2, 1.0)))
+    max_radius_y = int(round(np.random.normal(2, 1.0)))
+    while not (max_radius_x > 1 and max_radius_y > 1):
+        max_radius_x = int(round(np.random.normal(2, 1.5)))
+        max_radius_y = int(round(np.random.normal(2, 1.5)))
+    x = abs(min(int(round(np.random.normal(GRID_SIZE / 2.0, 0.3 * GRID_SIZE))), GRID_SIZE - 1))
+    y = abs(min(int(round(np.random.normal(GRID_SIZE / 2.0, 0.3 * GRID_SIZE))), GRID_SIZE - 1))
+    z = randint(0, NUM_FRAMES - 1)
+    frame_radius = randint(9, 16)
+    max_distance = sqrt(max_radius_x**2 + max_radius_y**2)
+    for i in range(-frame_radius, frame_radius):
+        for j in range(-max_radius_x, max_radius_x):
+            for k in range(-max_radius_y, max_radius_y):
+                a = x + j
+                if a < 0: a = 0
+                if a >= GRID_SIZE: a = GRID_SIZE - 1
+                b = y + k
+                if b < 0: b = 0
+                if b >= GRID_SIZE: b = GRID_SIZE - 1
+                if z + i > NUM_FRAMES - 1:
+                    break
+                distance = sqrt(j**2 + k**2)
+                brightness = randint(int((max_brightness - AVERAGE_BRIGHTNESS) / 2), max_brightness - AVERAGE_BRIGHTNESS)
+                brightness *= (1 - distance/max_distance)
+                brightness *= (1 - abs(i) / frame_radius)
+                I[z + i, a, b] += brightness
+                I_[z + i, a, b]  = brightness * (1 - distance/max_distance)**2
+
+if __name__=='__main__': generate_image()
